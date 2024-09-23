02521ad973c36382a0138a4f1c4e41bf00039561
da3monu <17841840392@163.com>
2024-09-23 18:29:34 +0800

    modified: 部署到github

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://daem0nu.netlify.app/",
-  base: "/",
+  site: "https://daem0nu.github.io",
+  base: "/astro-blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
