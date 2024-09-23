6b1471f7fff7b1fb07b9bec9de5b45c5fa363892
da3monu <17841840392@163.com>
2024-09-23 18:04:09 +0800

    modified: 把博客模板个性化

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Daem0nu',
+  subtitle: 'Blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -40,16 +40,16 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/daem0nu/astro-blog',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/liuhua-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Daem0nu',
+  bio: 'Enjoy your love,hack for fun.',
   links: [
     {
       name: 'Twitter',
@@ -61,12 +61,12 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/id/chtigali',
     },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/daem0nu',
     },
   ],
 }
--- src/content/posts/markdown-extended.md
+++ src/content/posts/markdown-extended.md
@@ -3,9 +3,9 @@ title: Markdown Extended Features
 published: 2024-05-01
 description: 'Read more about Markdown features in Fuwari'
 image: ''
-tags: [Demo, Example, Markdown, Fuwari]
-category: 'Examples'
-draft: false 
+tags: []
+category: Draft
+draft: true 
 ---
 
 ## GitHub repository cards
